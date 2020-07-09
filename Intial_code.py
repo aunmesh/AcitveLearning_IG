051991cc4386e81dd0eb2670b056c00b26ff472c
Rahul Jain <40121060+chabrarahul@users.noreply.github.com>
2020-07-09 03:06:33 -0400

    Update Intial_code.py

--- Intial_code.py
+++ Intial_code.py
@@ -15,36 +15,41 @@ torch.manual_seed(0)
 np.random.seed(0)
 torch.backends.cudnn.deterministic = True 
 torch.backends.cudnn.benchmark = False
+
 batch_size = 25
-k =10
-gradient = np.zeros(100000, dtype=float).reshape(10,10000)
-c_i = np.zeros(100000, dtype=float).reshape(10,10000)
-class_order = np.arange(100000).reshape(10,10000)
+k =10 #total number of class
+gradient = np.zeros(100000, dtype=float).reshape(10,10000) #Matrix of gradient. row = classes and columns = image
+c_i = np.zeros(100000, dtype=float).reshape(10,10000) #Matrix of gradient. row = classes and columns = image
+class_order = np.arange(100000).reshape(10,10000) #Matrix of gradient. row = classes and columns = image
 
 
+# function will return indices list for a batch 
 def batch_label(output, k):
-    sorting, indices = torch.sort(output, descending=True)
-    indices = indices.cpu().numpy() 
-    indices = indices[:,:k]
-    indices = np.transpose(indices)
+    sorting, indices = torch.sort(output, descending=True) # sorting stores the sorting probabolities and indices stores the class in descending order 
+    indices = indices.cpu().numpy() # converting tensor array to numpy array  
+    indices = indices[:,:k] # slicing the indices
+    indices = np.transpose(indices) # indices dimension = column = image and row = indices in descending order
     return indices
 
+# Stores the gradient, probablity and class order in matrix. 
+# grad = gradient of complete batch, output = model output, image_index = list of index of images, count = keeps the record of class 
 def complete_dict(grad, output,image_index, count, batch_size):
     sorting, indices = torch.sort(output, descending=True)    
     image_index = image_index.cpu().numpy()
     for batch_number in range(batch_size): 
-        mul = torch.norm(grad[batch_number], 2)
-        ind = image_index[batch_number]
-        gradient[count][ind] = mul.detach().cpu().numpy()         
-        c_i[count][ind] = sorting[batch_number][count].detach().cpu().numpy()         
-        class_order[count][ind] = indices[batch_number][count]
+        mul = torch.norm(grad[batch_number], 2) # calculates the norm of gradient of image with perticular class
+        ind = image_index[batch_number] # index of the image 
+        gradient[count][ind] = mul.detach().cpu().numpy()  # storing the value in gradient matrix        
+        c_i[count][ind] = sorting[batch_number][count].detach().cpu().numpy() # Storing the value probability in matrix        
+        class_order[count][ind] = indices[batch_number][count] # Storing the class in matrix 
 
 
-
+# Loading the data
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 train_set = torchvision.datasets.CIFAR10(root = './data/CIFAR10', train=True, transform=transform, download=True)
 test_set = torchvision.datasets.CIFAR10(root = './data/CIFAR10', train=False, transform = transforms.ToTensor(), download=True)
 
+# Adding index to dataset
 train_set_data = []
 count = 0
 for image in train_set:
@@ -60,6 +65,7 @@ for image in test_set:
     test_set_data.append(random)
     num = num + 1
 
+#Custom data loader with image index
 class Data(torch.utils.data.Dataset):
     def __init__(self, train_or_test,transform=None):
                 super(Data, self).__init__()
@@ -94,6 +100,7 @@ test_data_loader = torch.utils.data.DataLoader(test_data, batch_size= 25, shuffl
 dtype = torch.float
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+# model 
 class TemplateNet(nn.Module):
     def __init__(self):
         super(TemplateNet, self).__init__()
@@ -117,6 +124,7 @@ class TemplateNet(nn.Module):
 
 net = TemplateNet()
 
+# Training 
 net = net.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(net.parameters(), lr=1e-3, momentum=0.9)
@@ -136,7 +144,7 @@ for epoch in range(1):
             print("\n[epoch:%d, batch:%5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / float(12000)))
             running_loss = 0.0
 
-
+# Testing 
 for i, data in enumerate(test_data_loader):
     inputs, labels, image_index = data['image'],data['label'], data['index']
     inputs = inputs.to(device)
@@ -151,7 +159,7 @@ for i, data in enumerate(test_data_loader):
         net.zero_grad()
         index = torch.from_numpy(index).to(device)
         loss = criterion(output, index)
-        grad_input = torch.autograd.grad(loss, inputs,retain_graph = True, allow_unused=True)
+        grad_input = torch.autograd.grad(loss, inputs,retain_graph = True, allow_unused=True)# Calculating gradient of complete batch
         complete_dict(grad_input[0], output, image_index, counts, batch_size)
         counts = counts+1
     
